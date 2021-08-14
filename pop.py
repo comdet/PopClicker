4657241dbba08a2ac5588a46ef61cabc5faaeee6
comdet <listzone@hotmail.com>
2021-08-15 05:26:42 +0700

    update cooldown

--- pop.py
+++ pop.py
@@ -6,8 +6,8 @@ from random import randint
 scraper = cloudscraper.create_scraper()  # returns a CloudScraper instance
 
 token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJDb3VudHJ5Q29kZSI6IlRIIiwiQ291bnRyeU5hbWUiOiJUaGFpbGFuZCIsIklQIjoiMTgwLjE4My4xNTMuMjMiLCJJRCI6MzAsImV4cCI6MTYyOTA0OTU0M30.ZHk25AqVJcibQQI2_FhNYgOENmFcRL-zcQ5Njd3zc-Y"
-captcha_token = "03AGdBq26xxECcbynVH87SSmzzgsfciYr-URNZukFsfxpRv5P-eXqkNA4XjlZqMzmrnmD9X26qnQt40dC7Yk4y30dLTAMyIHd8KDcw_9wlEcmErG1wmdHRnsH4NN39aBV9zEgl46BzBn0bZYTlg2eMbXeczlXE31O2sQUwV6Fqf4gdRUj5G18xv0JZPrVhRkubHhzo23lWOSsvC5LSUpc_soleSWjBm9cv512aKx_3A1c1T0MifABbRcDR6y7GDeSF_F1iPedvAStYjdbkE7KOnBv67SZcKWAQORcJtoJKudBoYwMr6IzJAoNtu1KgifiUr9tOMPQ7SkF7NYQS8VVEKXGgbiJsoWimwBxrFvW5yTLtP-_WGN10R_RiV-SOWaop2JnuJqdiAZth3VowCJLA8rmLlXfu8LKNXZaZzzPOXMCVCf6YKebzkC8"
-for i in range(1000):
+captcha_token = "03AGdBq27u2XpAaLzTGbKTTTWbGM0ofRGfC79QEc933r5Jzml445tNYvSExkiod-6muplfyUHliz_pIUUH5jTTqffRY84N4a7i2YV2qIUs_1K7g42OwZlMzfpjrqmZ2jW8st74wTrwXewMOu1XnGC6QJ1erb8EbuX-6L1LwjiV2S4yOfX6IfaVrZO6KIsGPRfFGOzBz__hzVCL1WXei4ftwMFUnYm8ycCcVR4wZBwxuTC3jFh4hq6egtok2YHAcAZIwLk_dHK0gr1mznyCMRIwerYUMzBaNp1ZltTnP8E_0e4DSf7I29AlaKgQY-6yCtWmYwAZeGCDfI4hlpIInmj_w1atOIrCRdiSdYGL4NzgFJzXn2pMqEcYlt437cEcL2zRmzXgwhcvtTUM-QCpypRbUAg3_xRj6Prv-9C_gdX-lat0dKBN3l-nDIc"
+for i in range(9999999999):
 	url = f"https://stats.popcat.click/pop?pop_count=800&captcha_token={captcha_token}&token={token}"
 	res = scraper.get(url)
 	if (res.text.startswith("{") and res.text.endswith("}")) or res.status_code == 201:
@@ -17,4 +17,4 @@ for i in range(1000):
 	else:
 		pass
 		#print("PIP :( "+ str(res.status_code))
-	time.sleep(randint(100,2000)/1000) #delay a bit
+	time.sleep(30) #delay a bit
